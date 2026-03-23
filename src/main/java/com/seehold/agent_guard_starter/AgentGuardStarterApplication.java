a75f853b0f41ff3a6fbd06b0f1c166d1e8a90a30
15617171987 <3153525369@qq.com>
2026-03-23 21:52:27 +0800

    【feat】初始化

---
+++ src/main/java/com/seehold/agent_guard_starter/AgentGuardStarterApplication.java
@@ -0,0 +1,13 @@
+package com.seehold.agent_guard_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentGuardStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentGuardStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seehold/agent_guard_starter/AgentGuardStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seehold.agent_guard_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentGuardStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
